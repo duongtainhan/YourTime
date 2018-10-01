b06c9be3c94b6ff7fe227aed6af85020320b5362
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-10-01 21:44:25 +0700

    Initial design

---
+++ app/src/main/java/com/example/duongtainhan555/yourtime/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.duongtainhan555.yourtime;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(999);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
