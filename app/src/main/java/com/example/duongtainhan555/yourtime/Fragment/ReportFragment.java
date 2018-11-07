c8634ae79341197f27498f34ad89ac10a2bcb241
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-11-07 15:17:14 +0700

    "Animation + ColorText" for PieChart

--- app/src/main/java/com/example/duongtainhan555/yourtime/Fragment/ReportFragment.java
+++ app/src/main/java/com/example/duongtainhan555/yourtime/Fragment/ReportFragment.java
@@ -1,5 +1,6 @@
 package com.example.duongtainhan555.yourtime.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.duongtainhan555.yourtime.R;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
@@ -17,6 +19,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,7 +64,7 @@ public class ReportFragment extends Fragment {
         // gap of 2f
         entries.add(new BarEntry(5f, 70f));
         entries.add(new BarEntry(6f, 60f));
-        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        BarDataSet set = new BarDataSet(entries, "");
         //
         BarData data = new BarData(set);
         data.setBarWidth(0.9f); // set custom bar width
@@ -71,16 +74,28 @@ public class ReportFragment extends Fragment {
     }
     private void CreatePieChart()
     {
+        //pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+        //set animation
+        pieChart.animateY(1000,Easing.EasingOption.EaseInOutCubic);
+
         List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(8f, "Working time"));
+        entries.add(new PieEntry(16f, "Break time"));
 
-        entries.add(new PieEntry(18.5f, "Green"));
-        entries.add(new PieEntry(26.7f, "Yellow"));
-        entries.add(new PieEntry(24.0f, "Red"));
-        entries.add(new PieEntry(30.8f, "Blue"));
+        PieDataSet set = new PieDataSet(entries,"");
+        set.setSliceSpace(3f);
+        set.setSelectionShift(5f);
+        set.setColors(ColorTemplate.JOYFUL_COLORS);
 
-        PieDataSet set = new PieDataSet(entries, "Election Results");
         PieData data = new PieData(set);
+        data.setValueTextColor(R.color.text_pie_chart);
+        data.setValueTextSize(25f);
         pieChart.setData(data);
-        pieChart.invalidate(); // refresh
+        pieChart.invalidate();
     }
 }
