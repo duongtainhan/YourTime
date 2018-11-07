5b64ad043e183c14c5dfc657b1054897ae43919e
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-11-07 14:20:22 +0700

    Design Report Fragment + Demo

--- app/src/main/java/com/example/duongtainhan555/yourtime/Fragment/ReportFragment.java
+++ app/src/main/java/com/example/duongtainhan555/yourtime/Fragment/ReportFragment.java
@@ -10,12 +10,20 @@ import android.view.ViewGroup;
 
 import com.example.duongtainhan555.yourtime.R;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ReportFragment extends Fragment {
     BarChart barChart;
+    PieChart pieChart;
     View view;
     @Nullable
     @Override
@@ -28,23 +36,51 @@ public class ReportFragment extends Fragment {
     private void InitView()
     {
         barChart = view.findViewById(R.id.barChart);
+        pieChart = view.findViewById(R.id.pieChart);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        CreateBarChart();
+        CreateChart();
         super.onViewCreated(view, savedInstanceState);
     }
 
+    private void CreateChart()
+    {
+        CreateBarChart();
+        CreatePieChart();
+    }
     private void CreateBarChart()
     {
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(44f,0));
-        barEntries.add(new BarEntry(88f,1));
-        barEntries.add(new BarEntry(66f,2));
-        barEntries.add(new BarEntry(12f,3));
-        barEntries.add(new BarEntry(22f,4));
-        barEntries.add(new BarEntry(33f,5));
-        barEntries.add(new BarEntry(55f,6));
+
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(2f, 60f));
+        entries.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        entries.add(new BarEntry(5f, 70f));
+        entries.add(new BarEntry(6f, 60f));
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        //
+        BarData data = new BarData(set);
+        data.setBarWidth(0.9f); // set custom bar width
+        barChart.setData(data);
+        barChart.setFitBars(true); // make the x-axis fit exactly all bars
+        barChart.invalidate(); // refresh
+    }
+    private void CreatePieChart()
+    {
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(18.5f, "Green"));
+        entries.add(new PieEntry(26.7f, "Yellow"));
+        entries.add(new PieEntry(24.0f, "Red"));
+        entries.add(new PieEntry(30.8f, "Blue"));
+
+        PieDataSet set = new PieDataSet(entries, "Election Results");
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.invalidate(); // refresh
     }
 }
